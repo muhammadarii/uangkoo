ac827b019dd54d68682a08a6521e77488d969d5a
ciyek <aripurnomo606@gmail.com>
2025-05-09 23:57:00 +0700

    feat: Implement CategoryPage and integrate with MainPage navigation

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uangkoo/page/category_page.dart';
 import 'package:uangkoo/page/home_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,32 +12,72 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [HomePage(), CategoryPage()];
+  int currentIndex = 0;
+
+  void onTapTaped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CalendarAppBar(
-        accent: Colors.green,
-        backButton: false,
-        locale: "id",
-        onDateChanged: () {},
-        firstDate: DateTime.now().subtract(Duration(days: 140)),
-        lastDate: DateTime.now(),
+      appBar:
+          (currentIndex == 0)
+              ? CalendarAppBar(
+                accent: Colors.green,
+                backButton: false,
+                locale: "id",
+                onDateChanged: () {},
+                firstDate: DateTime.now().subtract(Duration(days: 140)),
+                lastDate: DateTime.now(),
+              )
+              : PreferredSize(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 50,
+                    horizontal: 16,
+                  ),
+                  child: Text(
+                    "Categories",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                preferredSize: Size.fromHeight(100),
+              ),
+      floatingActionButton: Visibility(
+        visible: (currentIndex == 0) ? true : false,
+        child: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+          shape: CircleBorder(),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.green,
-        child: Icon(Icons.add),
-        shape: CircleBorder(),
-      ),
-      body: HomePage(),
+      body: _children[currentIndex],
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            IconButton(
+              onPressed: () {
+                onTapTaped(0);
+              },
+              icon: Icon(Icons.home),
+            ),
             SizedBox(width: 20),
-            IconButton(onPressed: () {}, icon: Icon(Icons.list)),
+            IconButton(
+              onPressed: () {
+                onTapTaped(1);
+              },
+              icon: Icon(Icons.list),
+            ),
           ],
         ),
       ),
